109e74e846fdf8ce642415db6a3cba65c642503c
Tobinbv17 <48999634+Tobinbv17@users.noreply.github.com>
2020-10-26 10:32:23 -0400

    Update and rename EtaicollectAllSTARCountsFromFileDir.R to Etai_collectAllSTARCountsFromFileDir.R

--- Etai_collectAllSTARCountsFromFileDir.R
+++ Etai_collectAllSTARCountsFromFileDir.R
@@ -20,3 +20,22 @@ collectAllSTARCountsFromFileDir <- function(workdir="/czlab/etai/ExperimentsData
   htqc <- rbind(htqc, geneCounts=colSums(htmat))
   return(list(counts=htmat, qc=htqc))
 }
+
+                                        
+expr1 <- collectAllSTARCountsFromFileDir(workdir = rnaseqOutDir)
+qc <- expr1$qc
+counts <- expr1$counts
+names(counts) <- gsub(".R1.fastq", "", names(counts))
+names(qc) <- gsub(".R1.fastq", "", names(qc))
+
+#Generating QC data:
+qc2 <- round(sweep(qc, 2, colSums(qc), FUN = "/")*100)
+qcbygene <- rbind(colSums(counts>0), colSums(counts>4), colSums(counts>9))
+rownames(qcbygene) <- c("th1", "th5", "th10")
+
+qc3 <- t(rbind(qc2, qcbygene, totalReads=colSums(qc)))
+qc3 <- data.table(qc3)
+QC <- qc3
+QC <- data.table(sample_id = gsub(".ReadsPerGene.out.tab", "", colnames(qc)), QC)
+saveRDS(QC, file = "/singlecellcenter/etai/ExperimentsData/MOUSE_MELANOMA/data/Yao/H7GJWDRXX/QC.rds")
+
